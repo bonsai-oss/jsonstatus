80eda74adb9c4efe1fd69ea5159689297f7f72f9
Florian Bauer <florian@fsrv.xyz>
2022-10-09 17:18:14 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bonsai-oss/jsonstatus
+
+go 1.19
---
+++ model.go
@@ -0,0 +1,16 @@
+package jsonstatus
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Status struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (s *Status) Encode(w http.ResponseWriter) error {
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(s)
+}
